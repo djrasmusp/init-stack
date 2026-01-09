e06aa791c755a81ca1c975eb31b4d0b7d4b98933
Rasmus P <djrasmusp@users.noreply.github.com>
2026-01-09 23:04:50 +0100

    update package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    sourcemap: true,
+    target: "es2020",
+  },
+
+  server: {
+    port: 5173,
+    open: true,
+    strictPort: true,
+  },
 });
