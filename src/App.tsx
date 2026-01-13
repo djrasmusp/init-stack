f7ed36ec601191bdce964d183bc9d6f29121a42d
Rasmus P <djrasmusp@users.noreply.github.com>
2026-01-13 20:56:42 +0100

    feat: add text-box utility

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-box text-box-edge-end-alphabetic text-box-edge-start-cap text-box-trim-trim-both">
+        Vite + React
+      </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
